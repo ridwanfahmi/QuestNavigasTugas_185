6748248a934cbd63926d6be1b9f2dea5f427998b
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-08 01:04:14 +0700

    tambah logo, teks nama aplikasi, dan deskripsi di halaman awal

--- app/src/main/java/com/example/tugas5/view/Home.kt
+++ app/src/main/java/com/example/tugas5/view/Home.kt
@@ -1,14 +1,17 @@
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -40,6 +43,26 @@ fun HalamanAwal(
                 fontSize = 28.sp,
                 fontWeight = FontWeight.Light,
                 color = Color.Black
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Image(
+                painterResource(id = R.drawable.Babymonster),
+                contentDescription = "Babymonster",
+                modifier = Modifier.size(200.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                stringResource(id = R.string.nama),
+                fontSize = 36.sp,
+                fontWeight = FontWeight.ExtraBold,
+                color = blueTheme
+            )
+            Text(
+                text = stringResource(id = R.string.nim),
+                fontSize = 14.sp,
+                color = Color.Gray
             )
 
         }
