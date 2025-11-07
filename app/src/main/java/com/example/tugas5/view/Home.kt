bc2026042727b083751bdb309d72420be49feed0
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-08 01:01:41 +0700

    nambahin teks judul "Welcome" di halaman awal

--- app/src/main/java/com/example/tugas5/view/Home.kt
+++ app/src/main/java/com/example/tugas5/view/Home.kt
@@ -4,11 +4,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tugas5.R
 
 @Composable
 fun HalamanAwal(
@@ -30,6 +35,12 @@ fun HalamanAwal(
             modifier = Modifier.weight(1f),
             verticalArrangement = Arrangement.Center
         ){
+            Text(
+                stringResource(id = R.string.welcome),
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Light,
+                color = Color.Black
+            )
 
         }
     }
